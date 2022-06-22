fc9b666b4ae48ec3ef3cdf34d4e43b4ec6afad0d
Cravatec <samuel.wrobel@gmail.com>
2022-06-22 12:13:52 +0200

    Initial Commit

---
+++ Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Sam on 22/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

be1dc13e93f52e573ccf98688aefdc7da055b002
Cravatec <samuel.wrobel@gmail.com>
2022-06-24 14:35:18 +0200

    dismissKeyboard for Translate and Money page, add elements for the 3 pages

---
+++ Le Baluchon/Controller/TranslateViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TranslateViewController.swift
+//  Le Baluchon
+//
+//  Created by Sam on 24/06/2022.
+//
+
+import UIKit
+
+class TranslateViewController: UIViewController {
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        text1.resignFirstResponder()
+        text2.resignFirstResponder()
+
+    }
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+}
+}
--- Le Baluchon/Controller/WeatherViewController.swift
+++ Le Baluchon/Controller/WeatherViewController.swift
@@ -10,66 +10,32 @@ import UIKit
 class WeatherViewController: UIViewController {
 
     
+
     @IBOutlet weak var imageViewNY: UIImageView!
     @IBOutlet weak var imageViewParis: UIImageView!
     
     @IBOutlet weak var nyStackView: UIStackView!
+    @IBOutlet weak var parisStackView: UIStackView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let gifNY = UIImage.gifImageWithName("sun")
         imageViewNY.image = gifNY
         
-        let gifParis = UIImage.gifImageWithName("rain")
-        imageViewParis.image = gifParis
+//        let gifParis = UIImage.gifImageWithName("rain")
+//        imageViewParis.image = gifParis
         
         
         nyStackView.layer.cornerRadius = 10
         nyStackView.clipsToBounds = true
+        nyStackView.layer.borderWidth = 2.5
+        nyStackView.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
         
-//        // The following is "supposed" to create rounded corners for the stackview
-//            let subView = UIView()
-//            subView.backgroundColor = .yellow // this ends up showing through instead of the systemPink
-//            subView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//            subView.layer.cornerRadius = 10
-//            subView.layer.masksToBounds = true
-//            subView.clipsToBounds = true
-//            view.addSubview(subView)
-//            subView.translatesAutoresizingMaskIntoConstraints = false
-//
-//            let nyStackView = UIStackView()
-//        nyStackView.axis = .vertical
-//        nyStackView.distribution = .fillEqually
-//        nyStackView.alignment = .fill
-//        nyStackView.spacing = 6
-//        nyStackView.backgroundColor = .systemPink // this actually works
-//
-//            subView.addSubview(nyStackView)
-//        nyStackView.translatesAutoresizingMaskIntoConstraints = false
-            
-            // Fill the stackview with buttons
-//            for index in 0..< buttonList.count {
-//                let button = UIButton()
-//                button.setTitle(buttonList[index], for: .normal)
-//                button.backgroundColor = .cyan
-//                button.setTitleColor(.black, for: .normal)
-//                button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-//                stackView.addArrangedSubview(button)
-//            }
-//
-//            NSLayoutConstraint.activate([
-//                nyStackView.trailingAnchor.constraint(equalTo: subView.trailingAnchor),
-//                nyStackView.leadingAnchor.constraint(equalTo: subView.leadingAnchor),
-//                nyStackView.topAnchor.constraint(equalTo: subView.topAnchor),
-//                nyStackView.bottomAnchor.constraint(equalTo: subView.bottomAnchor),
-//
-//                subView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//                subView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//                subView.widthAnchor.constraint(equalToConstant: 140)
-//            ])
-        
-        
-        // Do any additional setup after loading the view.
+//        parisStackView.layer.cornerRadius = 10
+//        parisStackView.clipsToBounds = true
+//        parisStackView.layer.borderWidth = 5.5
+//        parisStackView.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
     }
     
 

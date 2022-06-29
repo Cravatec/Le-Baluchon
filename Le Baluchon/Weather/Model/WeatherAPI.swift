cd43e7122d1f32784cea7a8b5c315bb27be5fdc0
Cravatec <samuel.wrobel@gmail.com>
2022-06-29 15:23:26 +0200

    WeatherApi refactor add create separate method for building request, use URLComponent

--- Le Baluchon/Weather/Model/WeatherAPI.swift
+++ Le Baluchon/Weather/Model/WeatherAPI.swift
@@ -8,45 +8,63 @@
 import Foundation
 
 class WeatherAPI {
-
+    
     static var shared = WeatherAPI()
     private init() {}
     
-    private static let weatherNYUrl = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=40.712784&lon=-74.005941&appid=\(Apikey.weatherApiKey)&lang=fr")!
+    private let baseUrl = "https://api.openweathermap.org/data/2.5/weather"
     
-  //  private static let weatherParisUrl = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=48.87213&lon=2.33314&appid=\(Apikey.weatherApiKey)&lang=fr")!
-
     private var task: URLSessionDataTask?
-    private var session = URLSession(configuration: .default)
-
-func fetchWeather(callback: @escaping (Bool, Weather?) -> Void) {
-        // Request
-        var request = URLRequest(url: WeatherAPI.weatherNYUrl)
-        // Request Method
-        request.httpMethod = "POST"
+    private let session = URLSession(configuration: .default)
+    
+    private func buildNYWeatherRequest() -> URLRequest? {
+        var urlComponents = URLComponents(string: baseUrl)
+        urlComponents?.queryItems = [URLQueryItem(name: "lat", value: NYLocation.latitude),
+                                     URLQueryItem(name: "lon", value: NYLocation.longitude)]
         
-        let session = URLSession(configuration: .default)
+        if let url = urlComponents?.url {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            urlRequest.addValue(Apikey.weatherApiKey, forHTTPHeaderField: "appid")
+            return urlRequest
+        }
+        return nil
+    }
+    
+    
+    func fetchWeather(callback: @escaping (Bool, Weather?) -> Void) {
+        guard let request = buildNYWeatherRequest() else {
+            return
+        }
+        
         task = session.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                guard let data = data, error == nil else {
+            
+            guard let data = data, error == nil else {
                 callback(false, nil)
                 return
             }
-                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                    callback(false, nil)
-                    return
-                }
-                guard let responseJSON = try? JSONDecoder().decode([String: String].self, from: data),
-                      let main = responseJSON["main"],
-                      let description = responseJSON["description"],
-                      let icon = responseJSON["icon"] else {
-                        callback(false, nil)
-                        return
-               }
-                let weather = Weather(main: main, description: description, icon: icon)
-                callback(true, weather)
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                callback(false, nil)
+                return
             }
-            self.task?.resume()
+            
+            guard let responseJSON = try? JSONDecoder().decode([String: String].self, from: data),
+                  let main = responseJSON["main"],
+                  let description = responseJSON["description"],
+                  let icon = responseJSON["icon"] else {
+                callback(false, nil)
+                return
+            }
+            let weather = Weather(main: main, description: description, icon: icon)
+            callback(true, weather)
+        }
+        self.task?.resume()
     }
 }
+
+
+struct NYLocation {
+    static let latitude: String = "40.71278"
+    static let longitude: String = "-74.005941"
 }

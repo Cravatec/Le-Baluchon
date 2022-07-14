573c2702e9b9164627df2ae2396998c11a8ce52c
Samuel <92946129+Cravatec@users.noreply.github.com>
2022-07-14 15:27:16 +0200

    Layout update for Extensions, App & Scene Delegate, Tests

---
+++ Le Baluchon/Shared Model/extensions.swift
@@ -0,0 +1,59 @@
+//
+//  extensions.swift
+//  Le Baluchon
+//
+//  Created by Sam on 08/07/2022.
+//
+
+import Foundation
+import UIKit
+//
+//extension UIViewController {
+//    func cardViewBlackSetup() {
+//        WeatherViewController.cardViewBlack.layer.cornerRadius = 10
+//        WeatherViewController.cardViewBlack.clipsToBounds = true
+//   }
+//
+//    func cardViewWhiteSetup() {
+//        WeatherViewController.cardViewWhite.layer.cornerRadius = 10
+//        WeatherViewController.cardViewWhite.clipsToBounds = true
+//        WeatherViewController.cardViewWhite.layer.borderWidth = 1
+//        WeatherViewController.cardViewWhite.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
+//   }
+//}
+
+extension UIView {
+    func setCornerRadius(radius: CGFloat) {
+        self.layer.cornerRadius = radius
+    }
+    
+    func makeCornerRounded(cornerRadius: CGFloat, maskedCorners: CACornerMask){
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+        layer.borderWidth = 1
+        layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+}
+// MARK: - Extension UIViewController
+extension UIViewController {
+    func presentAlert(error: String) {
+        let alert = UIAlertController(title: "Erreur", message: error, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+extension MoneyViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        euroTextField.resignFirstResponder()
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if euroTextField.text == "" {
+            dollarTextField.text = "0.0"
+        }
+    }
+}

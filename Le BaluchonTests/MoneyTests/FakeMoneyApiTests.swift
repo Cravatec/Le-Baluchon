6626c4767e6310ac476a7abd2bb0d7ec76080e6e
Samuel <92946129+Cravatec@users.noreply.github.com>
2022-07-31 14:00:54 +0200

    Update FakeMoneyApiTests and FakeMoneyResponseData

--- Le BaluchonTests/MoneyTests/FakeMoneyApiTests.swift
+++ Le BaluchonTests/MoneyTests/FakeMoneyApiTests.swift
@@ -11,5 +11,80 @@ import XCTest
 
 class FakeMoneyApiTests: XCTestCase
 {
+    func testMoneyApiFetchJSONShouldPostFailedCallbackError() {
+        // Given
+        let moneyApi = MoneyApi(
+            session: URLSessionFake(data: nil, response: nil, error: FakeMoneyResponseData.error))
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change")
+        moneyApi.fetchMoney() { result in
+            // Then
+            switch result {
+            case .success:
+                XCTFail("We should have a failure case")
+            case .failure(let error):
+                XCTAssertNotNil(error)
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
 
+    func testMoneyApiFetchJSONShouldPostFailedCallbackIfNoData() {
+        // Given
+        let moneyApi = MoneyApi(
+            session: URLSessionFake(data: nil, response: nil, error: nil))
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change")
+        moneyApi.fetchMoney() { result in
+            // Then
+            switch result {
+            case .success(let money):
+            XCTAssertNil(money)
+            case .failure:
+            XCTAssertNil("We should have a success case")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+    func testMoneyApiFetchJSONShouldPostFailedCallbackIfIncorrectData() {
+        // Given
+        let moneyApi = MoneyApi(
+            session: URLSessionFake(data: FakeMoneyResponseData.moneyIncorrectData,
+                                    response: FakeMoneyResponseData.responseOK, error: nil))
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change")
+        moneyApi.fetchMoney() { result in
+            // Then
+            switch result {
+            case .success(let money):
+            XCTAssertNil(money)
+            case .failure(let error):
+            XCTAssertNotNil(error)
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+    }
+
+//    func testMoneyApiFetchJSONShouldPostSuccessCallbackIfNoErrorAndCorrectData() {
+//        let moneyApi = MoneyApi(
+//            session: URLSessionFake(data: FakeMoneyResponseData.moneyCorrectData,
+//                                    response: FakeMoneyResponseData.responseOK, error: nil))
+//        // When
+//        let expectation = XCTestExpectation(description: "Wait for queue change")
+//        moneyApi.fetchMoney() { result in
+//            // Then
+//            switch result {
+//            case .success():
+//                XCTAssertNil(Any?.self)
+//            case .failure(_):
+//                XCTAssertNotNil(MoneyResponse.self)
+//            }
+//            expectation.fulfill()
+//        }
+//        wait(for: [expectation], timeout: 0.01)
+//    }
 }
--- Le BaluchonTests/MoneyTests/FakeMoneyResponseData.swift
+++ Le BaluchonTests/MoneyTests/FakeMoneyResponseData.swift
@@ -9,5 +9,25 @@ import Foundation
 
 class FakeMoneyResponseData
 {
-    
+    // MARK: - Data
+    static var moneyCorrectData: Data? {
+        let bundle = Bundle(for: FakeMoneyResponseData.self)
+        let url = bundle.url(forResource: "FakeMoneyResponse", withExtension: "json")!
+        return try! Data(contentsOf: url)
+    }
+
+    static let moneyIncorrectData = "erreur".data(using: .utf8)!
+
+    // MARK: - Response
+    static let responseOK = HTTPURLResponse(
+        url: URL(string: "https://fixer.io")!,
+        statusCode: 200, httpVersion: nil, headerFields: [:])!
+
+    static let responseKO = HTTPURLResponse(
+        url: URL(string: "https://fixer.io")!,
+        statusCode: 500, httpVersion: nil, headerFields: [:])!
+
+    // MARK: - Error
+    class MoneyError: Error {}
+    static let error = MoneyError()
 }
